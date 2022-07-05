417a78b524737f5c6c9613911b8a19be74be5f40
Basteque <82060824+basilems@users.noreply.github.com>
2022-07-05 16:28:35 +0200

    Import changes

--- app/src/main/java/fr/atlasdev/chakchouscan/MainActivity.java
+++ app/src/main/java/fr/atlasdev/chakchouscan/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,10 +17,6 @@ import androidx.appcompat.app.AlertDialog;
 
 
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
